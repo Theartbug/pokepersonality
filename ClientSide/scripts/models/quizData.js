0bc2ea6a371f0ec3743aad6546c556b8505ad2be
G.Provost <grace.g.provost@gmail.com>
2018-01-05 13:38:49 -0800

    create question change animations.

--- ClientSide/scripts/models/quizData.js
+++ ClientSide/scripts/models/quizData.js
@@ -6,13 +6,14 @@ app = app || {};
     const quizData = {};
     
 
-    quizData.getMatch = (matchData) => {
+    quizData.getMatch = (matchData, cb) => {
         console.log(matchData);
         $.get(`${API_URL}/pokemon/match`, matchData)
             .then((result) => {
                 app.pokeBuilder.buildPoke(result);
                 app.quizView.displayResults();
-            });
+            })
+            .then(cb());
     };
 
     module.quizData = quizData;
--- ClientSide/scripts/views/quizView.js
+++ ClientSide/scripts/views/quizView.js
@@ -8,21 +8,31 @@ app = app || {};
     quizView.initQuiz = () => {
         quizView.changeQuestion();
         quizView.playAgain();
+        $('section').hide();
+        $('#type-question').show();
     };
 
     quizView.playAgain = () => {
         $('#play-again').on('click', function() {
-            app.quizControl.clearQuiz();
-            //TODO: hide results / show first question
+            $('#results').fadeOut(1000, app.quizControl.clearQuiz); //make sure the quiz is not cleared prematurely
+            $('#type-question').delay(500).fadeIn(1000);
         });
     };
 
     quizView.changeQuestion = () => {
         $('input[type="radio"]').on('click', function() {
             app.quizControl.combineSelections($(this).attr('name'), $(this).val().split(' '));
-            // TODO: jquery animation to move questions on and off screen
             if ($(this).parent().attr('id') === 'gender') { //if the last question was selected, send the data
-                app.quizData.getMatch(app.quizControl.selections);
+                app.quizData.getMatch(app.quizControl.selections, () => { //makes sure there is a result to show
+                    $('#gender-question').fadeOut(1000);
+                    $('#results').delay(500).fadeIn(1000);
+                });
+            } else {
+                $(this).parent().parent().slideUp('slow');
+                $(this).parent().parent().next().delay(400).slideDown('slow');
+
+                // $(this).parent().parent().hide('slide', {direction: 'left'}, 1000);
+                // $(this).parent().parent().next().delay(500).show('slide', {direction: 'right'}, 1000);
             }
         });
     };
